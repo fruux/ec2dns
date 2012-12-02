c9744c1617215d3bcfe1caaa133ae8c83fe8dd3e
Dominik Tobschall <dominik@fruux.com>
2012-12-02 20:58:12 +0100

    basic record caching

--- lib/ec2dns/ec2dns.php
+++ lib/ec2dns/ec2dns.php
@@ -16,6 +16,10 @@ class ec2dns {
 
     protected $ec2;
 
+    protected $dnsCache = array();
+
+    protected $ttl = 300;
+
     protected $listener = 'udp://127.0.0.1:54';
 
     /**
@@ -30,6 +34,41 @@ class ec2dns {
         $this->dns = new \Hoa\Dns(new \Hoa\Socket\Server($this->listener));
 
     }
+    /**
+     * dnsCache Setter
+     * @param string $type
+     * @param string $domain
+     * @param string $ip
+     *
+     * @return void
+     */
+    protected function setCache($type, $domain, $ip) {
+
+        $this->dnsCache[md5($type.$domain)] = array('ip' => $ip, 'created' => time());
+
+    }
+
+    /**
+     * dnsCaceh Getter
+     *
+     * @param  string $type
+     * @param  string $domain
+     * @return false|string
+     */
+    protected function getCache($type, $domain) {
+
+        if(isset($this->dnsCache[md5($type.$domain)])) {
+            if(time() - $this->ttl < $this->dnsCache[md5($type.$domain)]['created']) {
+                return $this->dnsCache[md5($type.$domain)];
+            } else {
+                unset($this->dnsCache[md5($type.$domain)]);
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
 
     /**
      * This method resolves tags to IPs via ec2host
@@ -37,15 +76,26 @@ class ec2dns {
      * @param string $tag
      * @return false|string
      */
-    protected function resolve($tag) {
+    protected function resolve($type, $domain) {
 
-        $ec2host = new ec2host(clone $this->ec2, $tag);
+        if($dnsCache = $this->getCache($type, $domain)) {
+            return $dnsCache['ip'];
+        } else {
+            $ec2host = new ec2host(clone $this->ec2, $domain);
 
-        if($ec2host->instances) {
-            return gethostbyname($ec2host->instances[0]['dnsName']);
-        };
+            if($ec2host->instances) {
+                $ip = gethostbyname($ec2host->instances[0]['dnsName']);
+                $this->setCache($type, $domain, $ip);
 
-        return false;
+                return $ip;
+            } else {
+
+                $this->setCache($type, $domain, false);
+                return false;
+
+            }
+
+        }
 
     }
 
@@ -58,7 +108,9 @@ class ec2dns {
     public function onQueryCallback(\Hoa\Core\Event\Bucket $bucket) {
 
         $data = $bucket->getData();
-        return $this->resolve($data['domain']);
+        //var_dump($data);
+
+        return $this->resolve($data['type'], $data['domain']);
 
     }
 
